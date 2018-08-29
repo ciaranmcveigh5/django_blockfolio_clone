a9beae86e308af9b9c2e55dc8c34f3d21b29a8f1
Ciaran McVeigh <ciaranmcveigh5@gmail.com>
2018-08-29 19:37:18 +0100

    changed the home page to point at the arbitrage page and added some headings and comments on the page

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('personal.urls')),
+    path('', include('arbitrage.urls')),
     path(r'blog/', include('blog.urls')),
     path(r'arbitrage/', include('arbitrage.urls')),
 ]
