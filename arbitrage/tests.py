a4d6f23e1fd11cdca8e971e3d9b3390f8cc63d20
Ciaran McVeigh <ciaranmcveigh5@gmail.com>
2018-09-07 23:33:55 +0100

    added basic tests for blockfolio and arbitrage

--- arbitrage/tests.py
+++ arbitrage/tests.py
@@ -1,3 +1,27 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .views import contact, blockfolio 
 
-# Create your tests here.
+class ArbitrageTests(TestCase):
+    def setUp(self):
+        url = reverse('contact')
+        self.response = self.client.get(url)
+
+    def test_arbitrage_view_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_arbitrage_url_resolves_contact_view(self): # change view name
+        view = resolve('/')
+        self.assertEqual(view.func, contact)
+
+class BlockfolioTests(TestCase):
+    def setUp(self):
+        url = reverse('blockfolio')
+        self.response = self.client.get(url)
+
+    def test_blockfolio_view_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/blockfolio')
+        self.assertEqual(view.func, blockfolio)
